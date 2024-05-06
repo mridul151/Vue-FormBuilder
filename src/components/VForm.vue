2a79bfb69a6db204bc32a5c343f7133f4f44c338
Mridul Kakkar <61381095+mridul151@users.noreply.github.com>
2024-05-06 10:20:08 +0530

    Update VForm.vue

--- src/components/VForm.vue
+++ src/components/VForm.vue
@@ -1,11 +1,44 @@
 <template>
-  <v-form>
-    <slot></slot>
-  </v-form>
+  <div>
+    <v-form>
+      <component
+        :is="componentName"
+        v-for="(component, index) in formComponents"
+        :key="index"
+        :textFieldProps="component"
+      ></component>
+      <v-btn color="primary" @click="handleSubmit">{{ buttonProps.text }}</v-btn>
+    </v-form>
+  </div>
 </template>
 
 <script>
+import VTextField from "./VTextField.vue";
+
 export default {
-  name: 'VForm'
-}
+  components: {
+    VTextField,
+  },
+  props: {
+    formComponents: {
+      type: Array,
+      required: true,
+    },
+    buttonProps: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    componentName() {
+      return "VTextField";
+    },
+  },
+  methods: {
+    handleSubmit() {
+      // Add your form submission logic here
+      console.log("Form submitted");
+    },
+  },
+};
 </script>
